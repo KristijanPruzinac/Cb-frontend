e763ebf6cc2060b28b8bf783df3414ab7429fcb6
Kikomali <kikomali2517@gmail.com>
2024-05-15 18:30:37 +0200

    Vecera :)

---
+++ js/cookie.js
@@ -0,0 +1,51 @@
+function SetCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function GetCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function GetComplexCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+
+    var finalCookie = "";
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            finalCookie = c.substring(name.length, c.length);
+            break;
+        }
+    }
+
+    return finalCookie.split(".");
+}
+
+function AreCookiesAccepted() {
+    const acceptCookiesValue = GetCookie("acceptCookies");
+    if (acceptCookiesValue == ""){
+        return false;
+    }
+    else {
+        return true;
+    }
+  }
